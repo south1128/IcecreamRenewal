587bcd44a7483878e7b07b85bbaa49130284c36b
J1 <south1128@gmail.com>
2022-12-02 22:06:07 +0900

    1202

---
+++ app/src/main/java/com/example/icecream_renual/Worker.java
@@ -0,0 +1,42 @@
+package com.example.icecream_renual;
+
+import static com.example.icecream_renual.Constants.A_MORNING_EVENT_TIME;
+import static com.example.icecream_renual.Constants.KOREA_TIMEZONE;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.WorkerParameters;
+
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+public class Worker extends androidx.work.Worker {
+    public Worker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        NotificationHelper mNotificationHelper = new NotificationHelper(getApplicationContext());
+        long currentMillis = Calendar.getInstance(TimeZone.getTimeZone(KOREA_TIMEZONE), Locale.KOREA).getTimeInMillis();
+
+        Calendar eventCal = NotificationHelper.getScheduledCalender(A_MORNING_EVENT_TIME);
+        long eventNotifyMinRange = eventCal.getTimeInMillis();
+
+        eventCal.add(Calendar.HOUR_OF_DAY,Constants.NOTIFICATION_INTERVAL_HOUR);
+        long eventNotifyMaxRange = eventCal.getTimeInMillis();
+
+        boolean iseventnotifyrange = eventNotifyMinRange <= currentMillis && currentMillis<= eventNotifyMaxRange;
+        boolean iseventNotifyAvailable = iseventnotifyrange;
+
+        if(iseventNotifyAvailable){
+            mNotificationHelper.createNotification(Constants.WORK_A_NAME);
+        }else {
+
+        }
+        return Result.success();
+    }
+}
